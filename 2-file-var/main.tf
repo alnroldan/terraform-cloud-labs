fe7ec04cc688a8b068450bbc86caadc3a092f66f
Jairo  Roldan <jairo.roldan@grupobabel.com>
2022-06-28 09:10:18 -0500

    utilizacion de variables

---
+++ 2-file-var/main.tf
@@ -0,0 +1,4 @@
+resource local_file "my-file" {
+  content = var.content
+  filename = var.filename
+}
---
+++ 2-file-var/variables.tf
@@ -0,0 +1,12 @@
+variable filename {
+  type        = string
+  description = "path and name of the file"
+}
+
+variable content {
+  type        = string
+  description = "file content"
+}
+
+
+
