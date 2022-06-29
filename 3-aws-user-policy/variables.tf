ace1c6a93dede3046f4b367d5d9b7285dce8ac4f
Jairo  Roldan <jairo.roldan@grupobabel.com>
2022-06-29 09:11:35 -0500

    aws user policy

---
+++ 3-aws-user-policy/variables.tf
@@ -0,0 +1,16 @@
+variable access_key {
+  type        = string
+  description = "access key aws"
+  sensitive = true
+}
+
+variable secret_key {
+  type        = string
+  description = "secret key aws"
+  sensitive = true
+}
+
+variable region {
+  type        = string
+  description = "region"
+}
